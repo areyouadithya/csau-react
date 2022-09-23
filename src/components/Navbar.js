5055ecb8d38ecffd79dff18c20a7e80098b2aa0e
r.navaneeth992 <r.navaneeth992@gmail.com>
2022-09-23 18:35:23 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="logo">
+                <div>
+                    <img src={'ig_short.png'}  alt="Instagram" />
+                </div>
+                <div>
+                    <img src={'ig_long.png'} alt="Instagram" />
+                </div>
+            </div>
+            <div className="icons">
+                <div>
+                    <img src={'home.png'} alt="Home" />
+                </div>
+                <div>
+                    <img src={'search.png'} alt="Search" />
+                </div>
+                <div>
+                    <img src={'heart.png'} alt="Like" />
+                </div>
+                <div>
+                    <img src={'send.png'} alt="Send" />
+                </div>
+            </div>
+            <button type="button" className="post-btn">
+                Add new post
+            </button>
+        </div>
+    )
+}
+
+export default Navbar;
