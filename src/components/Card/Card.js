0d50949c498d3ea26ff253a76b9f06202e376e29
r.navaneeth992 <r.navaneeth992@gmail.com>
2022-09-29 18:33:42 +0530

    final commit

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,17 @@
 import {RiDeleteBinFill} from 'react-icons/ri';
 import {BsPencilFill} from 'react-icons/bs';
 
-function Card() {
+const Card = ({details}) => {
+
+    let { location, name, image} = details;
     return (
         <div className="card">
-            <img className="card-img" src="https://images.unsplash.com/photo-1663915943941-9e3b66fe1e7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="image" />
+            <img className="card-img" src={image} alt="image" />
             <div className="card-body">
                 <img src="./panda-avatar.png" alt="User" id="user" />
                 <div className="card-desc">
-                    <div className="card-title">Gautam</div>
-                    <div className="card-text">Somewhere in CEG</div>
+                    <div className="card-title">{name}</div>
+                    <div className="card-text">{location}</div>
                 </div>
                 <div className="icons">
                     <RiDeleteBinFill id="trash" />
